3bd44e107e299a72248d435dfc6ed85c1c922a67
kienpm <manhkien1998@gmail.com>
2022-12-19 12:01:23 +0700

    initialize

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -13,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>hello world</div>
+      <div className='text-3xl font-bold underline'>Hell wrld</div>
     </>
   )
 }
